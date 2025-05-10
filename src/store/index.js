4929ceec06f5b585b1c60e86623f276c789d7e73
Oliver A. Kelton <oakelton@gmail.com>
2025-05-10 15:50:14 -0700

    Added leaderBoard slice to store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userProfileReducer from './userProfileSlice';
 import gameSettingsReducer from './gameSettingsSlice';
 import gameReducer from './gameSlice';
+import leaderBoardReducer from './leaderBoardSlice';
 
 export const store = configureStore({
   reducer: {
     userProfile: userProfileReducer,
     gameSettings: gameSettingsReducer,
     game: gameReducer,
+    leaderBoard: leaderBoardReducer,
   },
 });

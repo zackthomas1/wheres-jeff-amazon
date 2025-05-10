92b8da36076d83998cfd5cc69983f6afafb08c34
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-05-10 14:56:39 -0700

    Implement basic authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
+// index.js
+import React from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { AuthProvider } from "react-oidc-context";
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_YZDde7yWa",
+  client_id: "5rhuio0915e60k400o8qq3ajsm",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
+  <React.StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useAuth } from "react-oidc-context";
+
+function Home() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "5rhuio0915e60k400o8qq3ajsm";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+  );
+}
+
+export default Home;

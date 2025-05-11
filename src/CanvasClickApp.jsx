a0580e7af9f00c998c994b9c0cffbd6e6b829bad
Oliver A. Kelton <oakelton@gmail.com>
2025-05-10 17:40:21 -0700

    Integrated redux into CanvasCLickApp; added stub fetchNewGameRound function

--- src/CanvasClickApp.jsx
+++ src/CanvasClickApp.jsx
@@ -1,9 +1,12 @@
 import React, { useRef, useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+
+import { setNewGameRound } from './store/gameSlice';
 import imageSrcA from './assets/wheresjeff.png';
 
 const mockGameRounds = [
   {
-    image: imageSrcA,
+    imageSrc: imageSrcA,
     coordinates: { 
       x: 261,
       y: 400
@@ -11,8 +14,25 @@ const mockGameRounds = [
     toleranceRadiusX: 120,
     toleranceRadiusY: 220,
     difficulty: 'easy'
-  }
+  },
+  {
+    imageSrc: 'https://www.providencejournal.com/gcdn/authoring/2019/01/09/NPRJ/ghows-PJ-7f0e9188-00cb-1a2a-e053-0100007f329a-6d5b7ebf.jpeg?width=660&height=497&fit=crop&format=pjpg',
+    coordinates: { 
+      x: 200,
+      y: 200
+    },
+    toleranceRadiusX: 100,
+    toleranceRadiusY: 200,
+    difficulty: 'easy'
+  },
 ];
+
+// TODO: replace with API call
+const fetchNewGameRound = () => {
+  const index = Math.floor(Math.random() * mockGameRounds.length);
+
+  return mockGameRounds[index];
+};
 
 const ClickMessage = ({ status, message }) => {
   let spanStyle = null;
@@ -35,26 +55,56 @@ const ClickMessage = ({ status, message }) => {
 };
 
 const CanvasClickApp = () => {
+  const dispatch = useDispatch();
+  const game = useSelector(state => state.game);
   const canvasRef = useRef(null);
-  const [gameRound, setGameRound] = useState(mockGameRounds[0]);
   const [clickMessage, setClickMessage] = useState(null);
+  const [isClickEnabled, setIsClickEnabled] = useState(false);
+  const [headInstruction, setHeadInstruction] = useState(null);
 
   // const imageSrcA = "./assets/wheresjeff.png"; // place your image in the public folder or import it
+  const handleNewGameRound = (newGame) => {
+    dispatch(setNewGameRound(newGame));
+  };
 
+  // Get initial game round
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+    const gameRound = fetchNewGameRound();
 
-    const img = new Image();
-    img.src = gameRound.image;
-    img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
-    };
+    handleNewGameRound(gameRound);
   }, []);
 
+  // Initialize canvas every time a new game round is loaded
+  useEffect(() => {
+    if (game.length > 0)
+    {
+      // Get latest round
+      const gameRound = game[game.length - 1];
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+
+      const img = new Image();
+      img.src = gameRound.imageSrc;
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+      };
+
+      setIsClickEnabled(true);
+      setClickMessage(null);
+      setHeadInstruction("Find Jeff Amazon in the image");
+    }
+  }, [game]);
+
+  const gameRound = game[game.length - 1];
+
   const handleCanvasClick = (event) => {
+    if (! isClickEnabled)
+    {
+      return;
+    }
+
     const canvas = canvasRef.current;
     const rect = canvas.getBoundingClientRect();
 
@@ -67,6 +117,12 @@ const CanvasClickApp = () => {
     if ((Math.abs(x - targetX) < toleranceRadiusX) && (Math.abs(y - targetY) < toleranceRadiusY))
     {
       setClickMessage(<ClickMessage status="success" message="You found Jeff!" />);
+
+      setIsClickEnabled(false);
+      setHeadInstruction("Loading new image ...");
+      setTimeout(() => {
+        handleNewGameRound(fetchNewGameRound());
+      }, 1000);
     }
     else
     {
@@ -76,7 +132,7 @@ const CanvasClickApp = () => {
 
   return (
     <div>
-      <h2>Click on the image to get coordinates</h2>
+      <h2>{headInstruction}</h2>
       <canvas ref={canvasRef} onClick={handleCanvasClick} style={{ border: "1px solid black" }} />
       <p>
         {clickMessage}
